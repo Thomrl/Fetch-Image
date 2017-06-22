3473c6a8949c1ab7b1eb13646b3cff272b1a5f2f
Thomas Lauritzen <tommer.rl@live.dk>
2017-06-23 00:31:28 +0200

    added comments

--- fetch.py
+++ fetch.py
@@ -1,11 +1,11 @@
 #! Python3.5
 import os, shutil
 
-os.chdir("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Skyrim Special Edition")
+os.chdir("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Skyrim Special Edition") #Where you want to get the images from
 path = os.getcwd()
 print(path)
 
 for file in os.listdir():
-    if file.endswith(".png"):
-        shutil.move(file, "C:\\files\\Thomas\\Pictures\\Screenshots\\Skyrim")
+    if file.endswith(".png"): #The file type                                          #The file type
+        shutil.move(file, "C:\\files\\Thomas\\Pictures\\Screenshots\\Skyrim")         #Where you want the images to go
 
